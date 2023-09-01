5c874a7b3e284dff5eca43051c93876284965393
박민정 <ming1320@gmail.com>
2023-09-01 17:17:47 +0900

    230901 quiz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './pages/Main';
+import Detail from './pages/Detail';
+import { Route, Routes } from 'react-router-dom';
+
+import {useEffect, useState} from 'react';
+import QuizList from './components/QuizList';
+
+
 
 function App() {
+
+  
+  const [userName, setUserName] = useState("홍길동");
+  const [quizList, setQuizList] = useState(QuizList);
+  const quiz = [...QuizList]
+  const [selected, setSelected] = useState(3);
+  const [quizCnt, setQuizCnt] = useState(QuizList.length);
+  const [typeTxt, setTypetxt] = useState("전체");
+
+  const ChangeEvent = (data) =>{
+    const classValue = data.target.className;
+    const dataValue = data.target.value;
+    switch(true){
+      // includes("값") > 해당 문자열이 있는지 체크
+      case classValue.includes("name") :
+        setUserName(dataValue)
+      break;
+      case classValue.includes("random") :
+        
+        (dataValue === "1" ? setQuizList([...quizList].sort(()=>{
+          return Math.random() - 0.5}).slice(0, selected)
+          // 0 ~ 1사이의 값을 반환 > 0.5의 평균값을 얻게 되어 -0.5 ~ 0.5값으로 랜덤 출력
+        )
+        : setQuizList([...quizList]).slice(0, selected))
+        
+      break;
+      case classValue.includes("cnt") :
+        setSelected(dataValue);
+      break;
+      case classValue.includes("type") :
+        (dataValue === "전체"
+        ?
+        setQuizList([...QuizList].slice(0, selected))
+        :
+        setQuizList([...QuizList].filter((e)=>{
+          return e.type === dataValue
+        }).slice(0, selected))
+        )
+        setTypetxt(dataValue)
+        
+      break;
+      default:
+      console.log("데이터가 없습니다.")
+    }
+
+  }
+
+  
+
+  useEffect(()=>{
+    setQuizList([...QuizList].slice(0, selected));
+    setQuizCnt([...QuizList].filter((e)=>{
+      return typeTxt === "전체" ? true : e.type === typeTxt;
+    }).length);
+  },[typeTxt, selected])
+
+  console.log(quizList)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Main ChangeEvent={ChangeEvent} userName={userName} quiz={quiz} selected={selected} quizList={quizList} quizCnt={quizCnt} />} />
+        <Route path="/quiz" element={<Detail quizList={quizList} userName={userName} />}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/QuizList.js
@@ -0,0 +1,121 @@
+const QuizList = [
+    {
+        id: 1,
+        question : "html의 의미를 보기에서 고르세요",
+        answer : "Hyper Text Markup Language",
+        view: {
+            number1 : "Hyper Text Markup Language",
+            number2 : "Hyperlinks and Text Markup Language",
+            number3 : "Home Tool Markup Language",
+            number4 : "Hyperlinks Tool Markup Language"
+        },
+        hint: "하이퍼텍스트 마크업 언어입니다.",
+        type: "html"
+    },
+    {
+        id: 2,
+        question : " HTML의 특징으로 맞지 않는 것은?",
+        answer : "컴퓨터 시스템이나 운영체제에 종속적",
+        view: {
+            number1 : "웹 문서의 표준으로 지정",
+            number2 : "마크업 언어",
+            number3 : "ASCII 코드로 구성된 일반적인 텍스트 파일",
+            number4 : "컴퓨터 시스템이나 운영체제에 종속적"
+        },
+        hint: "",
+        type: "html"
+    },
+    {
+        id: 3,
+        question : "다음 속성에 대한 설명 중 틀린 것을 고르세요.",
+        answer : "alt 속성은 요소에 대한 설명으로 마우스를 호버하면 툴팁을 표시합니다.",
+        view: {
+            number1 : "src 속성은 필요한 소스의 경로를 나타냅니다.",
+            number2 : "href 속성은 하이퍼링크가 가리키는 URL을 나타냅니다.",
+            number3 : "alt 속성은 요소에 대한 설명으로 마우스를 호버하면 툴팁을 표시합니다.",
+            number4 : "lang 속성은 요소 내에 사용된 언어를 정의합니다"
+        },
+        hint: "",
+        type: "html"
+    },
+    {
+        id: 4,
+        question : "다음은 flex에 대한 설명입니다. 올바르지 않은 설명을 하는 사람을 고르세요.",
+        answer : "빙키 : flex-item들은 기본적으로 Axis 기준으로 정렬되기 때문에 기본 방향은 위쪽에서 아래쪽이야!",
+        view: {
+            number1 : "파이 : flex는 부모를 통해 자식들을 컨트롤할 수 있어!",
+            number2 : "썬 : 그리고 직계자식까지만 영향을 줄 수 있지.",
+            number3 : "빙키 : flex-item들은 기본적으로 Axis 기준으로 정렬되기 때문에 기본 방향은 위쪽에서 아래쪽이야!",
+            number4 : "뮤라 : flex-direction이 column일 때 가로로 중앙정렬을 하고 싶다면 align-item을 center로 설정하면 된대."
+        },
+        hint: "",
+        type: "css"
+    },
+    {
+        id: 5,
+        question : "다음 중 display:inline 에 대한 설명으로 올바른 내용을 고르세요.",
+        answer : "마진 탑(margin-top)값의 설정이 불가능하다.",
+        view: {
+            number1 : "줄 바꿈이 가능하다.",
+            number2 : "한 라인을 모두 차지하는 특징이 있다.",
+            number3 : "마진 탑(margin-top)값의 설정이 불가능하다.",
+            number4 : "display:inline 요소의 자식으로는 반드시 inline 요소만 가능하다."
+        },
+        hint: "",
+        type: "css"
+    },
+    {
+        id: 6,
+        question : "다음 중 display:inline 에 대한 설명으로 올바른 내용을 고르세요.",
+        answer : "마진 탑(margin-top)값의 설정이 불가능하다.",
+        view: {
+            number1 : "줄 바꿈이 가능하다.",
+            number2 : "한 라인을 모두 차지하는 특징이 있다.",
+            number3 : "마진 탑(margin-top)값의 설정이 불가능하다.",
+            number4 : "display:inline 요소의 자식으로는 반드시 inline 요소만 가능하다."
+        },
+        hint: "",
+        type: "css"
+    },
+    {
+        id: 7,
+        question : "쿠키는 어디에 저장되는가?",
+        answer : "사용자 로컬 컴퓨터의 파일",
+        view: {
+            number1 : "사용자 로컬 컴퓨터의 파일",
+            number2 : "사용자 로컬 컴퓨터의 데이터베이스",
+            number3 : "웹 서버 컴퓨터의 파일",
+            number4 : "웹 서버 컴퓨터의 데이터베이스"
+        },
+        hint: "",
+        type: "js"
+    },
+    {
+        id: 8,
+        question : "자바스크립트 객체를 생성할 때 사용하는 키워드는?",
+        answer : "new",
+        view: {
+            number1 : "creat",
+            number2 : "+",
+            number3 : "new",
+            number4 : "make"
+        },
+        hint: "",
+        type: "js"
+    },
+    {
+        id: 9,
+        question : "자바스크립트 객체를 생성할 때 사용하는 키워드는?",
+        answer : "new",
+        view: {
+            number1 : "creat",
+            number2 : "+",
+            number3 : "new",
+            number4 : "make"
+        },
+        hint: "",
+        type: "js"
+    },
+]
+
+export default QuizList
